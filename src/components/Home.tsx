76853b4b7ce545cd5ae31b0fec420acda2f0b574
Ayush Kumar <kumarayush6543@gmail.com>
2022-02-13 09:34:12 +0530

    updated

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,12 +2,8 @@ import React, { useState } from "react";
 import { Search } from "react-feather";
 import Carousel from "./Home/Carousel";
 // import "./Home.css";
-import Unsplash, { createApi } from "unsplash-js";
 
 const Home = () => {
-  const unsplash = createApi({
-    accessKey: "OjKLVNBzevtX2EKTHPzIk7YF8xC2iXjc-Is_XXh6tx0",
-  });
   const [medium, setState] = useState();
   return (
     <>
