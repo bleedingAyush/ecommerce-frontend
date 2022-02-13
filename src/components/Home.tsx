f9deaf61401a6c9ec61b93cd32faae3b9b978b6f
Ayush Kumar <kumarayush6543@gmail.com>
2022-02-13 09:41:28 +0530

    updated Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,6 @@ import Carousel from "./Home/Carousel";
 import Unsplash, { createApi } from "unsplash-js";
 
 const Home = () => {
-  const unsplash = createApi({
-    accessKey: "OjKLVNBzevtX2EKTHPzIk7YF8xC2iXjc-Is_XXh6tx0",
-  });
-  const [medium, setState] = useState();
   return (
     <>
       <Carousel />
