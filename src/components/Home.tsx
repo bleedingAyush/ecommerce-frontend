8d74f4c33b2aa323d23be7c5bba2e3945733f67e
Ayush Kumar <kumarayush6543@gmail.com>
2022-02-18 22:33:49 +0530

    kd

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Carousel from "./Home/Carousel";
 import Content from "./Home/Content";
+<<<<<<< HEAD
 // import "./Home.css";
+=======
+>>>>>>> 69526ee (rafactored home.tsx)
 
 const Home = () => {
   return (
